ee87b765bd842f35f4b4a7972e2fddebc20c7637
Paulo Cezario <ph.scezario@gmail.com>
2023-04-26 13:21:24 -0300

    Some adjusts

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-(function() {
+(function () {
     const header = document.querySelector('.ast-main-header-wrap'); // Enter menu class or ID
-    const adminBar = document.querySelector('#wpadminbar');
+    const adminBar = document.querySelector('#wpadminbar'); // Add upper bar class or ID
 
     let scrollposition = window.scrollY;
 
-    function menubar(){
+    function menubar() {
         scrollposition = window.scrollY;
 
-        if(scrollposition > 10){
-            header.classList.add("small-header"); 
-            if ( adminBar ) header.style.top = '32px';
-        }
-        else {
-            header.classList.remove("small-header");
-            if ( adminBar ) header.style.top = '0';
+        if (scrollposition > 10) {
+            header.classList.add('small-header');
+            if (adminBar) header.style.top = `${adminBar.style.height}px`;
+        } else {
+            header.classList.remove('small-header');
+            if (adminBar) header.style.top = '0';
         }
     }
 
-    window.addEventListener('scroll', function(){ 
+    window.addEventListener('scroll', function () {
         menubar();
     });
 
     menubar();
-
 })();
-
-
